157b9fccdf88a933962e0af0059b6951ea1a4224
nandhakumar <nandhakumar.s@ardhas.com>
2025-09-12 17:18:10 +0530

    Student model schema created

--- backend/models/Students.js
+++ backend/models/Students.js
@@ -1,7 +1,15 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
-mongoose.Schema({
-    __id:Number,
+const StudentSchema = mongoose.Schema({
+    _id:Number,
     name:{type:String, required: true},
     id_card:{type:String, required: true},
-})
+    class_section:{type:String, required: true},
+    mobile:{type:Number, required: true},
+    status:{type:Boolean, enum:[1,0], default:1, required:true},
+    trash:{type:String, enum:["YES","NO"], default:"NO", required:true}
+},
+{
+    timestamps:true,
+});
+export default mongoose.model("students", StudentSchema);
