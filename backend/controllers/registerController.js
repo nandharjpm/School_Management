ed2d954fb483dd56885afaac540483d5bfd52602
nandhakumar <76857790+nandharjpm@users.noreply.github.com>
2025-10-06 21:13:56 +0530

    register controller and model are created

---
+++ backend/controllers/registerController.js
@@ -0,0 +1,34 @@
+import bcrypt from "bcryptjs";
+import newUser from "../models/Register";
+import Register from "../../frontend/src/views/auth/Register";
+
+export const registerUser = async(req, res)=>{
+    try{
+        const {email, password, phone_number} = req.body;
+
+        if(!email || !password || !phone_number){
+            return res.status(400).json({mesage:"Please Enter All Fields"});
+        }
+
+        const existingUser = await newUser.findOne({email});
+        const exitsingPhoneNum = await newUser.findOne({phone_number});
+
+        if(existingUser || exitsingPhoneNum){
+            return res.status(400).json({message:"Email or Phone Number is alredy Exist"});
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const createUser = new Register({
+            email,
+            password:hashPassword,
+            phone_number,
+        });
+
+        await createUser.save();
+        return res.status(201).json({message:"User Created Successfully"});
+    }catch(err){
+        console.err(err);
+        return res.status(500).json({message:"Something went Wrong"});
+    }
+}
---
+++ backend/models/Register.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email:{type:String, required: true, unique: true},
+    password: {type:String, required: true},
+    phone_number: {type: String, required: true},
+},{
+    timestamps:true
+});
+
+const registerUser = mongoose.model("Register", userSchema);
+export default userSchema;
